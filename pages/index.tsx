21e58b677419ca4ea3620a3065b56e6784acd95f
Fluturecode <fluturecode@gmail.com>
2023-03-28 21:39:16 -0400

    added Connected component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,12 @@ import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import NavBar from "../components/NavBar"
 import Disconnected from "@/components/Disconnected"
+import { useWallet } from "@solana/wallet-adapter-react"
+import Connected from "../components/Connected"
 
 const Home: NextPage = () => {
-
+  const { connected } = useWallet()
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -24,11 +27,8 @@ const Home: NextPage = () => {
         <Stack w="full" h="calc(100vh)" justify="center">
 					<NavBar />
           <Spacer />
-          <Center>
-						<Disconnected/>
-          </Center>
+          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
           <Spacer />
-
           <Center>
             <Box marginBottom={4} color="white">
               <a
